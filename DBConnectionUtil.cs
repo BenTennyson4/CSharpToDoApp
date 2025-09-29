47ef0c7fc946b98241825e12c3e1212d12650c86
BenTennyson4 <151582101+BenTennyson4@users.noreply.github.com>
2025-09-29 16:45:43 -0500

    Changed Dockerfile and compose file to work with local database, and changed DBConnectionUtil to be able to correctly determine if app is being run locally or from container.

--- DBConnectionUtil.cs
+++ DBConnectionUtil.cs
@@ -12,16 +12,20 @@ public class DBConnectionUtility
 
         var dbSettings = configuration.GetSection("DBConnection");
 
+        // Choose the server depending on environment (set DOTNET_RUNNING_IN_DOCKER=true in Docker Compose)
+        var server = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_DOCKER") == "true"
+            ? dbSettings["Server_Docker"]
+            : dbSettings["Server_Host"];
+
         var builder = new SqlConnectionStringBuilder
         {
-            DataSource = dbSettings["Server"],
+            DataSource = server,
             InitialCatalog = dbSettings["Database"],
-            IntegratedSecurity = bool.Parse(dbSettings["IntegratedSecurity"] ?? "True"),
-            Encrypt = bool.Parse(dbSettings["Encrypt"] ?? "False"),
+            IntegratedSecurity = bool.Parse(dbSettings["IntegratedSecurity"] ?? "False"),
+            Encrypt = bool.Parse(dbSettings["Encrypt"] ?? "True"),
             TrustServerCertificate = bool.Parse(dbSettings["TrustServerCertificate"] ?? "True")
         };
 
-        // If there is a UserID provided, then try to use SQL ServerAuthentication instead of Windows authentication
         if (!string.IsNullOrEmpty(dbSettings["UserID"]))
         {
             builder.UserID = dbSettings["UserID"];
